6426e74744e32fefa6f43e1d63c19dd9bdf1c195
cuhte3 <farid@getportal.co>
2022-08-04 17:30:56 +0700

    Update PortalUITests.swift

--- Tests/PortalUITests/PortalUITests.swift
+++ Tests/PortalUITests/PortalUITests.swift
@@ -6,6 +6,5 @@ final class PortalUITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PortalUI().text, "Hello, World!")
     }
 }
