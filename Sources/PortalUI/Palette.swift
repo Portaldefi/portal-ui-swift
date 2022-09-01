f3272fa1c1dfa05fdc949a37f038f93638ab7bf9
cuhte3 <farid@getportal.co>
2022-09-01 16:41:39 +0700

    Create Palette.swift

---
+++ Sources/PortalUI/Palette.swift
@@ -0,0 +1,26 @@
+//
+//  Palette.swift
+//  
+//
+//  Created by farid on 9/1/22.
+//
+
+import SwiftUI
+
+public enum Palette {
+    static public let gradientLeadingColor = Color("gradient-1", bundle: .module)
+    static public let gradientTrailingColor = Color("gradient-2", bundle: .module)
+    static public let gray1 = Color("gray-1", bundle: .module)
+    static public let gray2 = Color("gray-2", bundle: .module)
+    static public let gray3 = Color("gray-3", bundle: .module)
+    static public let gray4 = Color("gray-4", bundle: .module)
+    static public let gray5 = Color("gray-5", bundle: .module)
+    static public let gray6 = Color("gray-6", bundle: .module)
+    static public let gray7 = Color("gray-7", bundle: .module)
+    static public let gray8 = Color("gray-8", bundle: .module)
+    static public let gray9 = Color("gray-9", bundle: .module)
+    static public let gray10 = Color("gray-10", bundle: .module)
+    static public let gray11 = Color("gray-11", bundle: .module)
+    static public let gray12 = Color("gray-12", bundle: .module)
+    static public let gray13 = Color("gray-13", bundle: .module)
+}
